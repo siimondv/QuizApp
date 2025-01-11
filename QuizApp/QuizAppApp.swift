2067cf06b7d98f5500eb1dc5aaab44a6d5e92a2c
Simon <simon.delgd@gmail.com>
2025-01-11 20:59:21 +0000

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Simon Delgado on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
